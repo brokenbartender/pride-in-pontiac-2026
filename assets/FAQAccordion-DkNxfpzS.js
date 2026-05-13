b8d602bc0899200a4c99540bbe0a87cb4633d2fb
Cody McKenzie <codymckenzie23@gmail.com>
2026-05-13 01:57:11 -0400

    Publish local Crofoot image asset

---
+++ assets/FAQAccordion-DkNxfpzS.js
@@ -0,0 +1,6 @@
+import{c as n,j as e}from"./index-9jGCBU60.js";/**
+ * @license lucide-react v0.300.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=n("ChevronDown",[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]]);function d({question:t,answer:s,isOpen:r,onClick:o}){return e.jsxs("div",{className:"border border-black/10 rounded-lg bg-white overflow-hidden mb-2",children:[e.jsxs("button",{className:"w-full text-left p-4 font-bold flex justify-between items-center",onClick:o,children:[e.jsx("span",{children:t}),e.jsx(a,{className:`transition-transform ${r?"rotate-180":""}`,size:20})]}),r&&e.jsx("div",{className:"p-4 pt-0 text-gray-500 border-t border-black/5 mt-2",children:s})]})}export{d as F};
