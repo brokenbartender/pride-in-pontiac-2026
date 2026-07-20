04209befba94ef10ed85f5df039a54a66eaeb7ca
Cody Mckenzie <codymckenzie23@gmail.com>
2026-07-20 12:48:09 -0400

    Update event website URL to custom domain

--- src/lib/eventLinks.ts
+++ src/lib/eventLinks.ts
@@ -2,7 +2,7 @@ export const EVENTBRITE_URL =
   "https://www.eventbrite.com/e/pride-in-pontiac-2026-still-here-still-proud-tickets-1989365764554";
 
 export const EVENT_WEBSITE_URL =
-  "https://brokenbartender.github.io/pride-in-pontiac-2026/";
+  "https://prideinpontiac.com/";
 
 export const SPONSOR_EMAIL =
   "mailto:baentertainmentMI@gmail.com?subject=Pride%20in%20Pontiac%202026%20Inquiry";
